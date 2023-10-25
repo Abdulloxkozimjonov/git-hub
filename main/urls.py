4ac4b1e98649384fc0903e388777a4279d005481
Abdulloxkozimjonov <kozimjonova00@gmail.com>
2023-10-25 20:25:33 +0600

    Abdullox

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index_view, name="index_url")
+    path('', index_view, name="index_url"),
+    path('task-creat/', create_task_view, name="create_task_url"),
+    path('task-delete/<int:pk>/', delete_task_view, name="delete_task_url"),
+    path('task-fininshed/<int:pk>/', finished_task_view, name="finished_task_url"),
+    path('status-finished/', finished_view, name="finished_url"),
+    path('task-delete/', delete_status_view, name="delete_status_url"),
+    path('task-Inprogress/', In_progress_view, name="In_progress_status_url"),
 ]
+
+git add .
+git commit -m "something"
+git push
--- main/views.py
+++ main/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request):
+    context={
+        'tasks': Todo.objects.all()
+    }
+    return render(request,'index.html',context)
+def create_task_view(request):
+    if request.method == "POST":
+        task= request.POST['task']
+        Todo.objects.create(
+            task=task
+        )
+        return redirect('index_url')
+    context={
+        'tasks':Todo.objects.all()
+    }
+    return render(request,'index.html',context)
+
+def delete_task_view(request,pk):
+    task= Todo.objects.get(pk = pk)
+    task.status = "DELETED"
+    task.save()
+    return redirect('index_url')
+
+def finished_task_view(request,pk):
+    task= Todo.objects.get(pk=pk)
+    task.status = "FINISHED"
+    task.save()
+    return redirect('index_url'),
+
+def finished_view(request):
+     task = Todo.objects.filter(status= "FINISHED")
+     context={
+         'task':task
+     }
+     return render(request,'Finished.html',context)
+
+
+def delete_status_view(request):
+    task = Todo.objects.filter(status = "DELETED")
+    context={
+        'task': task
+    }
+    return render(request,'delete_status.html',context)
+def In_progress_view(request):
+    task= Todo.objects.filter(status = "In progress")
+    context={
+        'task':task
+    }
+    return render(request,'in_progress.html',context)
