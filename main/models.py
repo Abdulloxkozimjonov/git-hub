83218449f289f064b0a4e7a9504e20ad35e655d5
mukhriddinuz <109203312+Muhriddinuz@users.noreply.github.com>
2023-10-27 15:20:07 +0500

    Mukhriddin

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    task = models.CharField(max_length=255)
+    status = models.CharField(max_length=255,default="In progress")
+
+    def str(self):
+        return self.task
